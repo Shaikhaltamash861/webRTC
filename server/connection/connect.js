57bb75c129a197e5cbc113c8af836c62e50af6f2
Shaikhaltamash861 <altamash2c2@gmail.com>
2023-09-09 17:11:31 +0530

    some change

--- server/connection/connect.js
+++ server/connection/connect.js
@@ -1,9 +1,9 @@
 const mongoose =require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGOURI)
+mongoose.connect('mongodb+srv://altamash2c2:altamash2c2@cluster0.koqh6pe.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>{
-    console.log('database is connected')
+    console.log('Database is connected')
 })
 .catch((err)=>{
     console.log(err)
